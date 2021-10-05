e64b245a95afb18903752dc0173ab64a4eb3808b
HienBlueskytechco <hiendao07062000@gmail.com>
2021-10-05 23:12:07 +0700

    check

---
+++ Subweb/Rooms/main.js
@@ -0,0 +1,10 @@
+$('.rooms-menu').slick({ 
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    arrows: false,
+    infinite: true,
+    pauseOnHover: true,
+    pauseOnFocus: false,
+  });
---
+++ main.js
@@ -0,0 +1,11 @@
+$('.autoplay').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  infinite: true,
+  speed: 1000,
+  fade: true,
+  cssEase: 'linear',
+  pauseOnHover: false,
+  pauseOnFocus: false,
+});
